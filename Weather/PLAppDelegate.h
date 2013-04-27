452812eeec5bd2eafa7db0984715a60d304c5af5
Paul Young <paulyoungonline@gmail.com>
2013-04-27 13:21:22 -0400

    Initial Commit

---
+++ Weather/PLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PLAppDelegate.h
+//  Weather
+//
+//  Created by Paul Young on 27/04/2013.
+//  Copyright (c) 2013 Paul Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
