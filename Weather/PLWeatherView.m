c6e956c69bc979cb910ba89c2fde6cbbf7535ab3
Paul Young <paulyoungonline@gmail.com>
2013-04-27 21:59:18 -0400

    Updated label positions.

--- Weather/PLWeatherView.m
+++ Weather/PLWeatherView.m
@@ -20,11 +20,11 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
-        self.temperatureLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 20, 160, 160)];
+        self.temperatureLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, 210, 100)];
         self.temperatureLabel.font = [UIFont fontWithName:@"PT Sans" size:100.0];
         [self addSubview:self.temperatureLabel];
         
-        self.timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 180, 160, 40)];
+        self.timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(18, 80, 200, 40)];
         self.timeLabel.font = [UIFont fontWithName:@"PT Sans" size:18.0];
         [self addSubview:self.timeLabel];
     }
